6d3328de21cebdb88ff31bae320ae1c94ff6df9e
bombody <135224248+bombody@users.noreply.github.com>
2023-08-24 21:58:38 +0000

    Updated interface

---
+++ C4.5/treePlotter.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2019/1/28 下午 09:02
+# @Author  : YuXin Chen
+
+import matplotlib.pyplot as plt
+
+decisionNode = dict(boxstyle="sawtooth", fc="0.8")
+leafNode = dict(boxstyle="round4", fc="0.8")
+arrow_args = dict(arrowstyle="<-")
+
+def plotNode(nodeTxt, centerPt, parentPt, nodeType):
+    createPlot.ax1.annotate(nodeTxt, xy=parentPt, xycoords='axes fraction', \
+                            xytext=centerPt, textcoords='axes fraction', \
+                            va="cent
